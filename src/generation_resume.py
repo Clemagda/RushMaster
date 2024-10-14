5fa63c40247c8f0f4afd6f5bf5c76df1aacb06d3
Clemagda <125815203+Clemagda@users.noreply.github.com>
2024-10-14 10:26:17 +0200

    Copied apple repo scripts in rushmaster script for model personnalization.

--- src/generation_resume.py
+++ src/generation_resume.py
@@ -1,12 +1,20 @@
 from transformers import AutoTokenizer,  AutoModelForCausalLM, LlamaTokenizer
 import torch
 
-# Lien du modèle :https://huggingface.co/Efficient-Large-Model/Llama-3-LongVILA-8B-128Frames/tree/main
-#
-# # Charger le modèle depuis le répertoire local
+# Lien du modèle
+
 # Remplace par le chemin du dépôt cloné
 model_path = "C:/Users/clema/Documents/RushMaster/Llama-3-LongVILA-8B-128Frames"
 model = AutoModelForCausalLM.from_pretrained(model_path)
 tokenizer = LlamaTokenizer.from_pretrained(model_path)
 
-# Ensuite, tu peux utiliser le modèle comme d'habitude
+# ----------------------------------
+# Normalement, LongVILA est kle modèle que je cherche, optimisé pour les vidéos longues.
+# Il est cendé me permettre directement de faire la génération de résumé "clefs en main" comme n'importe quel modèle.
+# Cela reste à tester, c'est pour ça que je me bats encore avec HF
+# ----------------------------------
+
+
+# ------------
+# Modèles potentiels :
+# - SlowFast
