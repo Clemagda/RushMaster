23b4904b08169d2eb7bd43847fa9f0b243c16b5e
Clemagda <125815203+Clemagda@users.noreply.github.com>
2024-10-31 11:09:24 +0100

    Modify for S3 I/O only.

--- src/csv_generation.py
+++ src/csv_generation.py
@@ -4,124 +4,106 @@ import os
 from main_processing import process_video
 from preprocessing import preprocess_video
 import boto3 # type: ignore
-from botocore.exceptions import ClientError # type: ignore
+from botocore.exceptions import ClientError, NoCredentialsError, PartialCredentialsError # type: ignore
 import pandas as pd # type: ignore
 
-def is_cloud_environment():
-    """
-    Vérifie l'environnement via une variable d'environnement.
-    Si ENVIRONMENT est défini sur "CLOUD", alors nous sommes sur le cloud (S3).
-    Sinon, c'est un environnement local.
-    """
-    return os.getenv("ENVIRONMENT", "LOCAL") == "CLOUD"
 
-s3 = boto3.client('s3') if is_cloud_environment() else None
+s3 = boto3.client('s3', region_name='eu-west-3')
 
-def get_video_files_from_directory(input_dir_or_bucket):
+INPUT_BUCKET_NAME = 'data-rushmaster'
+OUTPUT_BUCKET_NAME = 'data-rushmaster'
+INPUT_PREFIX = "Inputs/"
+OUTPUT_PREFIX = "Outputs/"
+
+def get_video_files_from_s3():
     """
-    Récupère tous les fichiers vidéo du dossier spécifié.
-
-    Args:
-        input_dir (str): Chemin du dossier contenant les fichiers vidéo.
-        
+    Récupère tous les fichiers vidéo du bucket S3 spécifié et les télécharge localement.
     Returns:
-        list: Liste des chemins des fichiers vidéo.
+        list: Liste des chemins des fichiers vidéo téléchargés.
     """
-    video_extensions = ['.mp4', '.mov', '.avi', '.mkv']  
+    video_extensions = ['.mp4', '.mov', '.avi', '.mkv']
     video_files = []
 
-    if is_cloud_environment():
-        try:
-            # Lister les objets dans le bucket S3 (par exemple, préfixe 'input/')
-            response = s3.list_objects_v2(Bucket=input_dir_or_bucket, Prefix='Inputs/')
-            for obj in response.get('Contents', []):
-                video_key = obj['Key']
+    try:
+        # Lister les objets dans le bucket S3 avec le préfixe défini
+        response = s3.list_objects_v2(Bucket=INPUT_BUCKET_NAME, Prefix=INPUT_PREFIX)
+        for obj in response.get('Contents', []):
+            video_key = obj['Key']
+            if any(video_key.endswith(ext) for ext in video_extensions):
+                # Créer /tmp si nécessaire
+                if not os.path.exists('/tmp'):
+                    os.makedirs('/tmp')
+                
                 local_video_path = f"/tmp/{os.path.basename(video_key)}"
                 
                 # Télécharger chaque vidéo depuis S3 dans le répertoire temporaire
+                print(f"Téléchargement de {video_key} vers {local_video_path}")
                 try:
-                    s3.head_object(Bucket=input_dir_or_bucket, Key=video_key)  # Vérifie si la vidéo existe dans S3
-                    s3.download_file(input_dir_or_bucket, video_key, local_video_path)
+                    s3.download_file(INPUT_BUCKET_NAME, video_key, local_video_path)
                     video_files.append(local_video_path)
                 except ClientError as e:
-                    print(f"Le fichier {video_key} n'existe pas dans S3 ou n'a pas pu être téléchargé : {e}")
-                    continue  
-
-        except ClientError as e:
-            print(f"Erreur lors de la récupération des fichiers depuis S3 : {e}")
+                    print(f"Erreur lors du téléchargement de {video_key} : {e}")
+                    continue
+    except ClientError as e:
+        print(f"Erreur lors de la récupération des fichiers depuis S3 : {e}")
     
-    else:
-    #Import local des vidéos
-        video_files = [os.path.join(input_dir_or_bucket, file) for file in os.listdir(input_dir_or_bucket) 
-                    if os.path.splitext(file)[1].lower() in video_extensions]
-        
     return video_files
 
-def create_csv_file(input_dir_or_bucket, output_csv, output_dir="Outputs", language="en"):
+def save_results_to_s3(dataframe, output_filename):
     """
-    Fusionne les résultats des trois scripts dans un fichier CSV.
+    Enregistre les résultats du traitement dans un fichier Excel, puis le télécharge dans le bucket S3.
     
     Args:
-        video_files (list): Liste des chemins des fichiers vidéo.
-        output_csv (str): Chemin du fichier CSV de sortie.
-        output_dir (str): Répertoire où sauvegarder les résultats intermédiaires.
-        language (str): Langue pour la transcription ('fr' pour français, 'en' pour anglais).
+        dataframe (pd.DataFrame): Le DataFrame contenant les résultats.
+        output_filename (str): Nom du fichier Excel de sortie.
     """
-           
-    video_files = get_video_files_from_directory(input_dir_or_bucket)
-    results=[]
+    output_path = f'/tmp/{output_filename}'
+    print(f"Enregistrement des résultats dans {output_path}")
 
-    #with open(output_csv, mode='w', newline='', encoding='utf-8') as csv_file:
-        # Définition des en-têtes des colonnes
-     #   fieldnames = ['video_id', 'quality_analysis', 'audio_transcription', 'video_summary']
-      #  writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        
-        #if csv_file.tell() ==0:
-         #   writer.writeheader()
+    # Enregistrer dans un fichier Excel localement avant de l'envoyer à S3
+    with pd.ExcelWriter(output_path) as writer:
+        dataframe.to_excel(writer, index=False)
 
-        # Parcourir chaque fichier vidéo
+    # Charger le fichier dans S3
+    try:
+        print(f"Téléversement de {output_filename} vers S3 : {OUTPUT_BUCKET_NAME}/{OUTPUT_PREFIX}{output_filename}")
+        s3.upload_file(output_path, OUTPUT_BUCKET_NAME, f'{OUTPUT_PREFIX}{output_filename}')
+        print(f"Fichier de résultats téléversé avec succès dans S3 : {OUTPUT_BUCKET_NAME}/{OUTPUT_PREFIX}{output_filename}")
+    except ClientError as e:
+        print(f"Erreur lors du téléversement du fichier de résultats dans S3 : {e}")
+
+def create_csv_file(language="en"):
+    """
+    Fusionne les résultats des traitements vidéos dans un fichier Excel et l'upload dans le bucket S3.
+    """
+    video_files = get_video_files_from_s3()
+    results = []
+
     for video in video_files:
         video_id = os.path.basename(video).split('.')[0]  # Utiliser le nom du fichier sans extension
 
-        try:     
-            print(f"===Prétraitement de la vidéo ===")          
+        try:
+            print(f"=== Prétraitement de la vidéo {video_id} ===")
             temp_video = preprocess_video(video)
-            print(f"=== prête ===")
-            
-            print(f"===Traitement de la vidéo ===")
-            result = process_video(temp_video, output_dir, language="en")
+            print(f"Prétraitement de {video_id} terminé, traitement en cours...")
+
+            result = process_video(temp_video, '/tmp', language=language)
             result['video_id'] = video_id
             results.append(result)
             os.remove(temp_video)
-       
+            print(f"Traitement de {video_id} terminé.")
+
         except Exception as e:
-            print(f"Erreur lors du traitement de la video  : {e}")
+            print(f"Erreur lors du traitement de la vidéo {video_id} : {e}")
 
     if results:
         df = pd.DataFrame(results)
-        output_path = output_csv 
-        df.to_excel(output_path, index=False)
-        print(f"Fichier Excel créé avec succès : {output_path}")
-
-        if is_cloud_environment():
-            s3.upload_file(output_csv, input_dir_or_bucket, f'outputs/{os.path.basename(output_csv)}')
+        save_results_to_s3(df, "results.xlsx")
+        print("Fichier Excel créé et téléversé avec succès.")
     else:
         print("Aucun résultat à enregistrer.")
-        
-    print(f"Les résultats ont été sauvegardés dans {output_csv} (ou dans le bucket S3).")
-
 
 if __name__ == "__main__":
-    if is_cloud_environment():
-        print("=====Execution dans un environnement Cloud=====")
-    else:
-        print("=====Execution dans un environnement local=====")
-
-    input_dir_or_bucket = "Inputs" if not is_cloud_environment() else "data-rushmaster"
-    output_csv = "Outputs/results.xlsx" if not is_cloud_environment() else "/tmp/results.xlsx"
-    output_dir = "Outputs" if not is_cloud_environment() else "/tmp"
-    language = "en"
-
-    print("=====Traitement des vidéos=====")
-    create_csv_file(input_dir_or_bucket, output_csv, output_dir, language)
-print("=====Fin du traitement=====")
+    print("===== Début du traitement des vidéos dans le bucket S3 =====")
+    create_csv_file(language="en")
+    print("===== Fin du traitement =====")
