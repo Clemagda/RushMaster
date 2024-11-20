5c2f2801c30c2c18893deee734447d2b476d9906
Clemagda <125815203+Clemagda@users.noreply.github.com>
2024-11-20 12:21:40 +0100

    Fix upload-service call and repository. Add logging info for upload-service api.

--- backend/upload/api.py
+++ backend/upload/api.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 import os
+import logging
+
+# Configuration du logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 # Répertoire de destination pour les fichiers téléchargés sur EFS
-UPLOAD_DIRECTORY = "/mnt/efs/inputs/"
+UPLOAD_DIRECTORY = "/app/shared/inputs"
 
 # Vérifier que le répertoire existe, sinon le créer
 if not os.path.exists(UPLOAD_DIRECTORY):
@@ -14,6 +19,9 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     try:
+        logger.info(
+            f"Réception du fichier : {file.filename}")
+
         # Créer le chemin complet pour enregistrer le fichier
         file_location = os.path.join(UPLOAD_DIRECTORY, file.filename)
 
@@ -21,8 +29,13 @@ async def upload_file(file: UploadFile = File(...)):
         with open(file_location, "wb") as f:
             f.write(await file.read())
 
+        logger.info(
+            f"Fichier '{file.filename}' téléversé avec succès à l'emplacement {file_location}.")
+
         return {"message": f"Fichier '{file.filename}' téléversé avec succès à l'emplacement {file_location}."}
 
     except Exception as e:
+        logger.error(f"Erreur lors du téléversement du fichier : {str(e)}")
         raise HTTPException(
-            status_code=500, detail=f"Erreur lors du téléversement du fichier : {str(e)}")
+            status_code=500, detail=f"Erreur lors du téléversement du fichier : {str(e)}"
+        )
--- frontend/frontend-st.py
+++ frontend/frontend-st.py
@@ -4,8 +4,19 @@ import requests
 import time
 
 # Configuration URLs et informations
-TRIGGER_LAMBDA_URL = "https://<API-ID>.execute-api.<region>.amazonaws.com/dev/trigger-eks"
-UPLOAD_SERVICE_URL = "http://<LOADBALANCER-URL>/upload"
+TRIGGER_LAMBDA_URL = "https://l02i7qlyzc.execute-api.eu-west-3.amazonaws.com/dev/trigger-eks"
+UPLOAD_SERVICE_URL = "http://a1679451e08a945bfb3b1f27b32fd628-2060507120.eu-west-3.elb.amazonaws.com:5000/upload/"
+PREPROCESSING_URL = "http://a886391d631da490d8c5429fb5b61ced-766592315.eu-west-3.elb.amazonaws.com:8000"
+
+# Initialiser les états dans la session
+if 'environment_ready' not in st.session_state:
+    st.session_state['environment_ready'] = False
+
+if 'files_uploaded' not in st.session_state:
+    st.session_state['files_uploaded'] = False
+
+if 'preprocessing_done' not in st.session_state:
+    st.session_state['preprocessing_done'] = False
 
 # Interface du frontend Streamlit
 st.title("Application de Traitement de Rush Vidéo - Téléversement des Vidéos")
@@ -14,28 +25,32 @@ st.title("Application de Traitement de Rush Vidéo - Téléversement des Vidéos
 uploaded_files = st.file_uploader("Téléversez vos fichiers vidéo ici", type=[
                                   "mp4", "mov", "avi", "mkv"], accept_multiple_files=True)
 
-if uploaded_files:
+# Préparation de l'environnement
+if uploaded_files and not st.session_state['environment_ready']:
     if st.button("Préparer l'environnement de traitement"):
         with st.spinner("Préparation de l'environnement..."):
+            # Appel de la fonction Lambda pour démarrer les pods
             response = requests.post(TRIGGER_LAMBDA_URL, json={
-                                     "user_id": "user123"})
+                                     "action": "start"})
             if response.status_code == 200:
                 st.success("Environnement prêt ! Les pods ont été démarrés.")
+                st.session_state['environment_ready'] = True
+
             else:
                 st.error(
                     f"Erreur lors de l'initialisation des pods: {response.text}")
-            # Attendre que les pods soient opérationnels (temps à adapter)
-            time.sleep(20)
 
+# Téléversement des fichiers (uniquement si l'environnement est prêt)
+if st.session_state['environment_ready'] and not st.session_state['files_uploaded']:
     if st.button('Téléverser les fichiers pour le traitement'):
+        all_uploaded = True
         for uploaded_file in uploaded_files:
             with st.spinner(f"Téléversement de {uploaded_file.name} en cours ..."):
                 try:
-                    files = {'file': uploaded_file.getvalue()}
-                    headers = {'user-id': "user123",
-                               'file-name': uploaded_file.name}
+                    files = {'file': (uploaded_file.name,
+                                      uploaded_file, uploaded_file.type)}
                     upload_response = requests.post(
-                        UPLOAD_SERVICE_URL, files=files, headers=headers)
+                        UPLOAD_SERVICE_URL, files=files)
 
                     if upload_response.status_code == 200:
                         st.success(
@@ -43,25 +58,36 @@ if uploaded_files:
                     else:
                         st.error(
                             f"Erreur lors du téléversement de {uploaded_file.name} : {upload_response.text}")
+                        all_uploaded = False
                 except Exception as e:
                     st.error(f"Erreur lors du téléversement : {e}")
+                    all_uploaded = False
 
-    # Bouton pour déclencher le prétraitement
+        if all_uploaded:
+            st.session_state['files_uploaded'] = True
+
+# Déclenchement du prétraitement (uniquement si les fichiers sont téléversés)
+if st.session_state['files_uploaded'] and not st.session_state['preprocessing_done']:
     if st.button("Lancer le traitement"):
         with st.spinner("Patience, le traitement est en cours ..."):
-            response = requests.post(
-                f"http://{UPLOAD_SERVICE_URL}/preprocess/")
-            if response.status_code == 200:
-                st.success(
-                    "Le traitement est terminé.")
-            else:
-                st.error(
-                    f"Erreur lors du déclenchement du service de prétraitement : {response.text}")
+            try:
+                # 1h de traitement prévue
+                response = requests.post(
+                    f"{PREPROCESSING_URL}/preprocess/", timeout=3600)
+                if response.status_code == 200:
+                    st.success("Le traitement est terminé.")
+                    st.session_state['preprocessing_done'] = True
+                else:
+                    st.error(
+                        f"Erreur lors du déclenchement du service de prétraitement : {response.text}")
+            except Exception as e:
+                st.error(f"Erreur lors du déclenchement : {e}")
 
-    # Bouton pour télécharger le fichier Excel généré
+# Téléchargement du fichier Excel généré (uniquement si le prétraitement est terminé)
+if st.session_state['preprocessing_done']:
     if os.path.exists("./shared/outputs/results.xlsx"):
         with open("./shared/outputs/results.xlsx", "rb") as file:
-            btn = st.download_button(
+            st.download_button(
                 label="Télécharger le fichier Excel généré",
                 data=file,
                 file_name="results.xlsx",
